17882b519db383824c3d5c345cd1f92be7bc4239
SH <qhuongcse19@gmail.com>
2025-04-19 15:51:04 +0700

    Update: add click here to can move to the tracing page on local browser

--- front-end/traceability/src/components/GenerateQRCode/GenerateQRCode.tsx
+++ front-end/traceability/src/components/GenerateQRCode/GenerateQRCode.tsx
@@ -10,10 +10,15 @@ interface GenerateQRCodeProps extends AppBodyProps {
 const GenerateQRCode: React.FC<GenerateQRCodeProps> = ({ packageKey, organization }) => {
   const url = `http://${LOCAL_IP}:${APP_PORT}${TRACING_ENDPOINT}?organization=${organization}&packageKey=${packageKey}`;
 
+  const handleLinkClick = () => {
+    const queryLocalUrl = `http://localhost:${APP_PORT}${TRACING_ENDPOINT}?organization=${organization}&packageKey=${packageKey}`
+    window.open(queryLocalUrl, '_blank');
+  };
+
   return (
     <div>
       <p style={{ fontSize: '20px', fontWeight: 'bold' }}>
-      Scan this QR code to visit the tracing page on your phone:
+      Click <span style={{ color: 'blue', textDecoration: 'underline', cursor: 'pointer' }} onClick={handleLinkClick}>here</span> or scan this QR code to visit the tracing page on your phone:
       </p>
       <QRCodeSVG value={url} size={256} level="H" />
     </div>
