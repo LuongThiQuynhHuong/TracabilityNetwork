73135f1aa8b065433d20d7e86fc3f8acea0a5e00
SH <qhuongcse19@gmail.com>
2025-04-23 13:37:08 +0700

    Update org profiles for backend

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,49 +1,49 @@
 
 let profile = {
     regulatoryDepartment: {
-        "cryptoPath": "../../network/organizations/peerOrganizations/regulatoryDepartment.com", 
-		"keyDirectoryPath": "../../network/organizations/peerOrganizations/regulatoryDepartment.com/users/User1@regulatoryDepartment.com/msp/keystore/",
-        "certPath":     "../../network/organizations/peerOrganizations/regulatoryDepartment.com/users/User1@regulatoryDepartment.com/msp/signcerts/cert.pem",
-		"tlsCertPath":  "../../network/organizations/peerOrganizations/regulatoryDepartment.com/peers/peer0.regulatoryDepartment.com/tls/ca.crt",
+        "cryptoPath": "../../network/organizations/peerOrganizations/regulatoryDepartment.ptracing.com", 
+		"keyDirectoryPath": "../../network/organizations/peerOrganizations/regulatoryDepartment.ptracing.com/users/User1@regulatoryDepartment.ptracing.com/msp/keystore/",
+        "certPath":     "../../network/organizations/peerOrganizations/regulatoryDepartment.ptracing.com/users/User1@regulatoryDepartment.ptracing.com/msp/signcerts/cert.pem",
+		"tlsCertPath":  "../../network/organizations/peerOrganizations/regulatoryDepartment.ptracing.com/peers/peer0.regulatoryDepartment.ptracing.com/tls/ca.crt",
 		"peerEndpoint": "localhost:7051",
-		"peerHostAlias":  "peer0.regulatoryDepartment.com",
-        "mspId": "RegulatoryDepartmentMSP"
+		"peerHostAlias":  "peer0.regulatoryDepartment.ptracing.com",
+        "mspId": "regulatoryDepartmentMSP"
     },
     farm: {
-        "cryptoPath": "../../network/organizations/peerOrganizations/farm.com", 
-		"keyDirectoryPath": "../../network/organizations/peerOrganizations/farm.com/users/User1@farm.com/msp/keystore/",
-        "certPath":     "../../network/organizations/peerOrganizations/farm.com/users/User1@farm.com/msp/signcerts/cert.pem",
-		"tlsCertPath":  "../../network/organizations/peerOrganizations/farm.com/peers/peer0.farm.com/tls/ca.crt",
-		"peerEndpoint": "localhost:8051",
-		"peerHostAlias":  "peer0.farm.com",
-        "mspId": "FarmMSP"
+        "cryptoPath": "../../network/organizations/peerOrganizations/farm.ptracing.com", 
+		"keyDirectoryPath": "../../network/organizations/peerOrganizations/farm.ptracing.com/users/User1@farm.ptracing.com/msp/keystore/",
+        "certPath":     "../../network/organizations/peerOrganizations/farm.ptracing.com/users/User1@farm.ptracing.com/msp/signcerts/cert.pem",
+		"tlsCertPath":  "../../network/organizations/peerOrganizations/farm.ptracing.com/peers/peer0.farm.ptracing.com/tls/ca.crt",
+		"peerEndpoint": "localhost:9051",
+		"peerHostAlias":  "peer0.farm.ptracing.com",
+        "mspId": "farmMSP"
     },
     processor: {
-        "cryptoPath": "../../network/organizations/peerOrganizations/processor.com", 
-		"keyDirectoryPath": "../../network/organizations/peerOrganizations/processor.com/users/User1@processor.com/msp/keystore/",
-        "certPath":     "../../network/organizations/peerOrganizations/processor.com/users/User1@processor.com/msp/signcerts/cert.pem",
-		"tlsCertPath":  "../../network/organizations/peerOrganizations/processor.com/peers/peer0.processor.com/tls/ca.crt",
-		"peerEndpoint": "localhost:9051",
-		"peerHostAlias":  "peer0.processor.com",
-        "mspId": "ProcessorMSP"
+        "cryptoPath": "../../network/organizations/peerOrganizations/processor.ptracing.com", 
+		"keyDirectoryPath": "../../network/organizations/peerOrganizations/processor.ptracing.com/users/User1@processor.ptracing.com/msp/keystore/",
+        "certPath":     "../../network/organizations/peerOrganizations/processor.ptracing.com/users/User1@processor.ptracing.com/msp/signcerts/cert.pem",
+		"tlsCertPath":  "../../network/organizations/peerOrganizations/processor.ptracing.com/peers/peer0.processor.ptracing.com/tls/ca.crt",
+		"peerEndpoint": "localhost:8051",
+		"peerHostAlias":  "peer0.processor.ptracing.com",
+        "mspId": "processorMSP"
     },
     distributor: {
-        "cryptoPath": "../../network/organizations/peerOrganizations/distributor.com", 
-		"keyDirectoryPath": "../../network/organizations/peerOrganizations/distributor.com/users/User1@distributor.com/msp/keystore/",
-        "certPath":     "../../network/organizations/peerOrganizations/distributor.com/users/User1@distributor.com/msp/signcerts/cert.pem",
-		"tlsCertPath":  "../../network/organizations/peerOrganizations/distributor.com/peers/peer0.distributor.com/tls/ca.crt",
-		"peerEndpoint": "localhost:10051",
-		"peerHostAlias":  "peer0.distributor.com",
-        "mspId": "DistributorMSP"
+        "cryptoPath": "../../network/organizations/peerOrganizations/distributor.ptracing.com", 
+		"keyDirectoryPath": "../../network/organizations/peerOrganizations/distributor.ptracing.com/users/User1@distributor.ptracing.com/msp/keystore/",
+        "certPath":     "../../network/organizations/peerOrganizations/distributor.ptracing.com/users/User1@distributor.ptracing.com/msp/signcerts/cert.pem",
+		"tlsCertPath":  "../../network/organizations/peerOrganizations/distributor.ptracing.com/peers/peer0.distributor.ptracing.com/tls/ca.crt",
+		"peerEndpoint": "localhost:11051",
+		"peerHostAlias":  "peer0.distributor.ptracing.com",
+        "mspId": "distributorMSP"
     },
     retailer: {
-        "cryptoPath": "../../network/organizations/peerOrganizations/retailer.com", 
-		"keyDirectoryPath": "../../network/organizations/peerOrganizations/retailer.com/users/User1@retailer.com/msp/keystore/",
-        "certPath":     "../../network/organizations/peerOrganizations/retailer.com/users/User1@retailer.com/msp/signcerts/cert.pem",
-		"tlsCertPath":  "../../network/organizations/peerOrganizations/retailer.com/peers/peer0.retailer.com/tls/ca.crt",
-		"peerEndpoint": "localhost:11051",
-		"peerHostAlias":  "peer0.retailer.com",
-        "mspId": "RetailerMSP"
+        "cryptoPath": "../../network/organizations/peerOrganizations/retailer.ptracing.com", 
+		"keyDirectoryPath": "../../network/organizations/peerOrganizations/retailer.ptracing.com/users/User1@retailer.ptracing.com/msp/keystore/",
+        "certPath":     "../../network/organizations/peerOrganizations/retailer.ptracing.com/users/User1@retailer.ptracing.com/msp/signcerts/cert.pem",
+		"tlsCertPath":  "../../network/organizations/peerOrganizations/retailer.ptracing.com/peers/peer0.retailer.ptracing.com/tls/ca.crt",
+		"peerEndpoint": "localhost:10051",
+		"peerHostAlias":  "peer0.retailer.ptracing.com",
+        "mspId": "retailerMSP"
     }
 }
 module.exports = { profile }
